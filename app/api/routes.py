1499bf1f0c5072a25f7f43055e1613827761bb8a
Tung Xuan <tungxuan101998@gmail.com>
2021-02-18 17:15:23 +0700

    complete classify

---
+++ app/api/routes.py
@@ -0,0 +1,41 @@
+from app.api import bp
+from app.api.keras_open_nsfw import predict
+import os
+from flask import request, jsonify
+import time
+from app.main.utils import make_response, is_base64
+import base64
+
+
+cwd = os.getcwd()
+model = predict.load_model(f'{cwd}/app/api/keras_open_nsfw/nsfw_mobilenet2.h5')
+
+
+@bp.route('/classify_nsfw', methods=['GET', 'POST'])
+def classify_photo_nsfw():
+    if request.method == 'GET':
+        return make_response(False, description='The get method is not available')
+
+    data = request.json
+    if 'base64_image' not in data:
+        return make_response(False, description='Image is empty!')
+
+    base64_image = data['base64_image'].replace('data:image/jpeg;base64,', '')
+    if not isinstance(base64_image, str) or not is_base64(base64_image):
+        return make_response(False, description='Base64 string format is incorrect')
+
+    imgdata = base64.b64decode(base64_image)
+    image_path = './app/api/image.jpg'
+    with open(image_path, 'wb') as f:
+        f.write(imgdata)
+
+    # start = time.time()
+    dict_result = predict.classify(model, image_path)
+    # print(f'Model classify in {time.time() - start} seconds')
+    softmax = dict_result[image_path]
+    label = max(softmax, key=softmax.get)
+    result = {'NSFW': 0}
+    if label == 'porn' or label == 'hentai':
+        result['NSFW'] = 1
+    # result['Classify'] = {'Label': label, 'Detail': softmax}
+    return make_response(True, result, '')
---
+++ app/main/utils.py
@@ -0,0 +1,30 @@
+from typing import Dict
+from flask import json, jsonify
+import base64
+
+
+def make_response(is_success=True, params=None, description=''):
+    if is_success:
+        response = dict()
+        response['Result'] = 'Success'
+        response['Description'] = description
+        if params is not None and isinstance(params, dict):
+            response.update(params)
+
+        return jsonify(response)
+    else:
+        return jsonify({'Result': 'Failed', 'Description': description})
+
+
+def is_base64(sb):
+    try:
+        if isinstance(sb, str):
+            # If there's any unicode here, an exception will be thrown and the function will return false
+            sb_bytes = bytes(sb, 'ascii')
+        elif isinstance(sb, bytes):
+            sb_bytes = sb
+        else:
+            raise ValueError("Argument must be string or bytes")
+        return base64.b64encode(base64.b64decode(sb_bytes)) == sb_bytes
+    except Exception:
+        return False
