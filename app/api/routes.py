043aff82e250197dd7a8cf9d1c161a82d731e189
Tung Xuan <tungxuan101998@gmail.com>
2021-02-19 15:59:05 +0700

    Upload file multipart form data

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,46 +1,72 @@
 from app.api import bp
 from app.api.keras_open_nsfw import predict
 import os
-from flask import request, jsonify
+from flask import request
 import time
-from app.main.utils import make_response, is_base64
-import base64
+from app.main.utils import make_response
+from werkzeug.utils import secure_filename
+from PIL import Image
+from io import BytesIO
 
 
-current_dir_path = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(current_dir_path, 'keras_open_nsfw/nsfw_mobilenet2.h5')
-image_path = os.path.join(current_dir_path, 'image.jpg')
+CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(CURRENT_PATH, 'keras_open_nsfw/nsfw_mobilenet2.h5')
+IMAGE_UPLOAD_FOLDER = os.path.join(CURRENT_PATH, '../../logs/image_upload')
+IMAGE_PATH = os.path.join(CURRENT_PATH, 'image.jpg')
+IMAGE_ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-model = predict.load_model(model_path)
-predict.classify(model, image_path)
+model = predict.load_model(MODEL_PATH)
+predict.classify(model, IMAGE_PATH)
 
 
-@bp.route('/classify_nsfw', methods=['GET', 'POST'])
+@bp.route('/nsfw/check', methods=['GET', 'POST'])
 def classify_photo_nsfw():
     if request.method == 'GET':
         return make_response(False, description='The get method is not available')
 
-    data = request.json
-    if 'base64_image' not in data:
-        return make_response(False, description='Image is empty!')
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        return make_response(False, description='File not found!')
+    image_file = request.files['file']
 
-    base64_image = data['base64_image'].replace('data:image/jpeg;base64,', '')
-    if not isinstance(base64_image, str) or not is_base64(base64_image):
-        return make_response(False, description='Base64 string format is incorrect')
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if image_file.filename == '':
+        return make_response(False, description='File not found!')
 
-    imgdata = base64.b64decode(base64_image)
-    with open(image_path, 'wb') as f:
-        f.write(imgdata)
+    if image_file and allowed_file(image_file.filename):
+        pass
+    else:
+        return make_response(False, description='Invalid file format!')
+
+    image_file.stream.seek(0)
+    image_file.save(IMAGE_PATH)
 
     start = time.time()
-    dict_result = predict.classify(model, image_path)
+    dict_result = predict.classify(model, IMAGE_PATH)
 
-    softmax = dict_result[image_path]
+    softmax = dict_result[IMAGE_PATH]
     label = max(softmax, key=softmax.get)
     result = {'NSFW': 0}
     if label == 'porn' or label == 'hentai':
         result['NSFW'] = 1
 
-    print(f'Model classify in {time.time() - start} seconds. Result: {result}')
-    # result['Classify'] = {'Label': label, 'Detail': softmax}
+    filename = secure_filename(image_file.filename)
+    basename, ext = os.path.splitext(filename)
+    basename += time.strftime("_%Y%m%d_%H%M%S")
+    basename += "_SFW" if result['NSFW'] == 0 else '_NSFW'
+    filename = basename + ext
+    image_file.stream.seek(0)
+    # image_file.save(os.path.join(IMAGE_UPLOAD_FOLDER, filename))
+    image_bytes = BytesIO(image_file.stream.read())
+    im = Image.open(image_bytes)
+    im_resize = im.resize((224, int(im.height * 224 / im.width)))
+    im_resize.save(os.path.join(IMAGE_UPLOAD_FOLDER, filename))
+    print(f'Model classify in {time.time() - start} seconds. Result: {result}. File: {filename}')
+
     return make_response(True, result, '')
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in IMAGE_ALLOWED_EXTENSIONS
